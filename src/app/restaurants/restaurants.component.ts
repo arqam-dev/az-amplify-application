49895a78f80371d86e03f6564ca7b5332b819f83
arqam-tkxel <48586137+arqam-dev@users.noreply.github.com>
2023-09-11 18:05:04 +0500

    added subscription to show the data real-time
    zen observers

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { APIService, Restaurant } from '../API.service';
-
+/** Subscription type will be inferred from this library */
+import { ZenObservable } from 'zen-observable-ts';
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.scss']
 })
-export class RestaurantsComponent implements OnInit {
+export class RestaurantsComponent implements OnInit, OnDestroy {
 
   public createForm: FormGroup;
  /* declare restaurants variable */
  public restaurants: Array<Restaurant> = [];
+/** Declare subscription variable */
+private subscription: ZenObservable.Subscription | null = null;
 
   constructor(private api: APIService, private fb: FormBuilder) {
     this.createForm = this.fb.group({
@@ -32,12 +35,26 @@ export class RestaurantsComponent implements OnInit {
         console.log('error creating restaurant...', e);
       });
   }
-
   ngOnInit(): void {
+
+      /* subscribe to new restaurants being created */
+  this.subscription = this.api.OnCreateRestaurantListener.subscribe(
+    (event: any) => {
+      const newRestaurant = event.value.data.onCreateRestaurant;
+      this.restaurants = [newRestaurant, ...this.restaurants];
+    }
+  );
+
         /* fetch restaurants when app loads */
         this.api.ListRestaurants().then((event) => {
           this.restaurants = event.items as Restaurant[];
         });
   }
 
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = null;
+  }
 }
